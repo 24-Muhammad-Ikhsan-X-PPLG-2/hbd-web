4c7de4797c91d0cb3dc54b3274d1e1ad42de30fb
ikhsanm181209 <ikhsanm181209@gmail.com>
2025-12-19 03:53:25 +0000

    Update page file

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useState, useEffect } from "react";
 import { PartyPopperExplosion } from "@/components/PartyPopperExplosion";
 import { HeroSection } from "@/components/HeroSection"
